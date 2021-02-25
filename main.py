07a3aaacd7bfba4daf8c02bd3aa4bf91d3c43d05
Nolan Ardolino <nolanardolino@Macintosh.local>
2021-02-24 19:31:12 -0800

    hello

---
+++ main.py
@@ -0,0 +1,14 @@
+from torchvison import utils
+from basic_fcn import *
+from dataloader import *
+from utils
+import torchvision
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import time
+import torch.nn
+import matplotlib.pyplot as plt
+
+
+# assemble the train, test, and val sets
---
+++ model_class.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+class Model(nn.Module):
+    
+    def __init__(self, n_class):
+        super().__init()
+        self.n_class = n_class
+
+
+        self.classifier = 
+        #return nothing
+
+
+    def forward(self, x):
+
+
+        
+        score = self.classifier()
+
+        return score # this is a 
+
